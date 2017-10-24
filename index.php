883b97c924ac872fb199511e34bf9911769744e3
Andrew Johnston <arjohnston3@gmail.com>
2017-10-24 09:06:37 -0700

    Add meta tag for mobile responsiveness

--- index.php
+++ index.php
@@ -82,6 +82,7 @@ else if(!empty(substr($_SERVER['REQUEST_URI'],13)))
 		<title>URL Shortner</title>
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/font-awesome-4.6.3/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body>
 
